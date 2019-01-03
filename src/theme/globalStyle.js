4ae19cd29b09015f08febc6233b7cb2a54d7aea1
github username <crishanks12@gmail.com>
2019-01-02 21:54:49 -0700

    created paragraph, font and padding

---
+++ src/theme/globalStyle.js
@@ -0,0 +1,11 @@
+import { injectGlobal } from 'styled-components';
+
+/* eslint disable */
+injectGlobal `
+    @import url('https://fonts.googleapis.com/css?family=Raleway');
+    
+    body {
+        font-family: 'Raleway', sans-serif;
+        font-size: 20px;
+    }
+`
