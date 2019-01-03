243c5be7c035cacdf7d83aca5ef5222e588302f4
github username <crishanks12@gmail.com>
2019-01-02 20:58:25 -0700

    finish router setup, add homepage

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          Hey, I'm Cris.
-        </header>
+      <div>
+        {this.props.children}
       </div>
     );
   }
