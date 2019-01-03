22f0294a6caf1bc472b9c25122340fe4cbda5a73
github username <crishanks12@gmail.com>
2019-01-03 12:34:06 -0700

    reorganized folders, added reveal element

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component, propTypes } from 'react';
-import styled from 'styled-components';
-import Container from '../theme/grid/container';
+import {Container} from '../../theme/grid';
+import { 
+    Image,
+    HeroImage,
+    RevealP
 
-const Image = styled.img`
-    width: 100%;
-`
+} from './Home.style';
 
 export default class Home extends Component {
     static propTypes = {};
@@ -25,13 +26,14 @@ export default class Home extends Component {
     render() {
         return (
             <Container>
-                <h1>Hey, I'm Cris.</h1>
-                <p>{this.state.data}</p>
-                <button onClick={this.increment.bind(this)}>Increment</button>
-                <Image src={require('../assets/headshot-cris.jpg')} alt="headshot image"/>
-                <p>
+                <HeroImage>
+                    <h1>Cris Hanks</h1>
+                    <h2>Full Stack Developer</h2>
+                </HeroImage>
+                {/* <Image src={require('../assets/headshot-cris.jpg')} alt="headshot image"/> */}
+                <RevealP hide={false}>
                     At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
-                    </p>
+                </RevealP>
             </Container>
         );
     }
