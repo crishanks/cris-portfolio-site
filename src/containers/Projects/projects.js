7c4c9b4e678ab445a40ceda7c437703aafb2efc0
github username <crishanks12@gmail.com>
2019-01-03 17:04:55 -0700

    react zoom library no longer functional

--- src/containers/Projects/projects.js
+++ src/containers/Projects/projects.js
@@ -1,30 +1,11 @@
 import React, { Component } from "react";
-import Zoomy from 'react-zoomy'; 
-import {
-    ImageButton
-} from './projects.style';
 import { Container } from '../../theme/grid';
 
 class Projects extends Component {
     render() {
         return (
             <Container>
-                <Zoomy
-                    imageUrl={require('../../../src/assets/coffee-hand.jpg')}
-                    renderThumbnail={({ showImage }) => 
-                        <ImageButton onClick={showImage}>
-                            <img src={require('../../assets/coffee-hand-thumbnail.jpg')} 
-                            alt="coffe hand thumbnail"/>
-                        </ImageButton>
-                    }
-                        scale={[1.1, 1.1]}
-                        imageProps={{
-                            style: {
-                            width: '100vw',
-                            height: 'auto'
-                            }
-                        }}
-                    />
+                <h1>React-Zoomy no longer available.</h1>
             </Container>
         );
     }
